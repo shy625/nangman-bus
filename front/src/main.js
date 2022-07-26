ccd6e3c63d55126646d9f62486aefe8a20a7fc18
woohree <woohree2@gmail.com>
2022-07-26 17:24:44 +0900

    [fix] directory structure bug fix..

--- front/src/main.js
+++ front/src/main.js
@@ -1,11 +1,10 @@
 import { createApp, h } from 'vue'
 import App from './App.vue'
 
-import router from './common/lib/vue-router'
-import store from './common/lib/store'
-import VueAxios from './common/lib/axios'
-import axios from './common/lib/axios'
-
+import router from './router/vue-router'
+import store from './store/store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/display.css'
