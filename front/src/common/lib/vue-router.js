8b4f9d7d10bc40a76b0a6a31f4031e0e6d2079fe
woohree <woohree2@gmail.com>
2022-07-26 09:54:16 +0900

    [feature] directory structure setting

--- front/src/common/lib/vue-router.js
+++ front/src/common/lib/vue-router.js
@@ -13,10 +13,10 @@ const router = createRouter({
     { name: 'accounts', path: '/accounts/:userId', component: AccountsView },
     { name: 'reports', path: '/reports/:userId', component: ReportsView },
     // { name: 'reportDetail', path: '/reports/:userId/reportId', component: ReportDetailView },
-    { name: 'room', path: '/room/:roomId', component: RoomView },
+    { name: 'chat', path: '/chat/:roomId', component: ChatView },
     // 방명록이랑 유저목록은 컴포넌트 구조에 따라 달라질 예정
     // { name: 'board', path: '/room/board', component: BoardView },
     // { name: 'users', path: '/room/users', component: UsersView }
   ]
 })
-export default router;
+export default router
