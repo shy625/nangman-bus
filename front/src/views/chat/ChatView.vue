554749214bf43301c25c9af4d06750c0b5c5ef4d
woohree <woohree2@gmail.com>
2022-07-27 16:05:48 +0900

    [feature] chat header style complete

--- front/src/views/chat/ChatView.vue
+++ front/src/views/chat/ChatView.vue
@@ -1,58 +1,70 @@
 <template>
+  <header>
+    <div class="chat-header">
+      <div class="chat-header-back">&lt; 1</div>
+      <div class="chat-header-title">5006번 버스</div>
+    </div>
+    <hr class="chat-header-hr">
+  </header>
+
   <el-carousel 
-  indicator-position="none" 
   height="100vh"
+  indicator-position="none" 
+  initial-index="1"
+  arrow="always"
+  :autoplay="false"
+  :loop="false"
   >
     <el-carousel-item>
       <BoardList></BoardList>
     </el-carousel-item>
-        <el-carousel-item>
+    <el-carousel-item>
       <ChatRoom></ChatRoom>
     </el-carousel-item>
-        <el-carousel-item>
+    <el-carousel-item>
       <UserList></UserList>
     </el-carousel-item>
   </el-carousel>
-
-  <!-- <div class="container">
-    <div class="pages">
-      <div class="board-list">
-        <button @onClick="slide('next')">다음</button>
-        <BoardList></BoardList>
-      </div>
-      <div class="chat-room">
-        <button @onClick="slide('prev')">이전</button>
-        <button @onClick="slide('next')">다음</button>
-        <ChatRoom></ChatRoom>
-      </div>
-      <div class="user-list">
-        <button @onClick="slide('prev')">이전</button>
-        <button @onClick="slide('next')">다음</button>
-        <UserList></UserList>
-      </div>
-    </div>
-  </div> -->
 </template>
 
 <script setup>
-<<<<<<< HEAD
   import BoardList from './components/BoardList.vue'
   import ChatRoom from './components/ChatRoom.vue'
   import UserList from './components/UserList.vue';
-=======
->>>>>>> 804d654b0bdf0ecef66ebdee6eee6556d0954838
 
 </script>
 
 <style>
-  .el-carousel {
-    height: auto;
-  }
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
+/* body App.vue로 옮기기 */
+body {
+  margin: 0;
+  font-family: BMHANNAAir;
+}
+.el-carousel__arrow {
+  top: 75% !important;
+  opacity: 0.3;
+  background-color: #FFD96A !important;
+}
+.el-carousel__arrow:hover {
+  opacity: 1;
+}
+.chat-header {
+  display: flex;
+  padding: 20px 0px;
+  margin: 0px 20px;
+  position: relative;
+  justify-content: center;
+  font-size: 1.5rem;
+}
+.chat-header-back {
+  position: absolute;
+  left: 12px;
+}
+.chat-header-title {
+  color: #F34949;
+}
+.chat-header-hr {
+  margin: 0px 32px;
+}
 
-  .el-carousel__item:nth-child(2n + 1) {
-    background-color: #d3dce6;
-  }
 </style>
