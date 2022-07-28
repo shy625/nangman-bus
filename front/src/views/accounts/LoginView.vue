ce5dd1da608cc69bfe494ca4a09eaacb6087b2ed
Bermyu <1004romeo@naver.com>
2022-07-28 16:44:07 +0900

    [feature] login developed

--- front/src/views/accounts/LoginView.vue
+++ front/src/views/accounts/LoginView.vue
@@ -5,46 +5,50 @@
   <div>
     <h3>이메일을 입력해주세요.</h3>
   </div>
-  <div>
+  <form @submit.prevent="login(credentials)">
     <div>
-      <div>이메일</div>
-      <input type="email" value="이메일을 입력해주세요">
+      <label for="email">이메일</label><br>
+      <input v-model="credentials.email" id="email" type="email" autofocus placeholder="이메일을 입력해주세요" required />
     </div>
     <div>
-      <div>비밀번호</div>
-      <input type="password" value="비밀번호를 입력해주세요">
+      <label for="password">비밀번호</label><br>
+      <input v-model="credentials.password" id="password" type="password" placeholder="비밀번호를 입력해주세요" required />
     </div>
     <button>로그인</button>
-    <a href="https://127.0.0.1/">회원가입</a>
-  </div>
+  </form>
+  <a href="https://127.0.0.1/">회원가입</a>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 
 export default {
-    name: '',
-    components: {
-        
-    },
-    data() {
-        return {
-            sampleData: ''
-        }
-    },
-    setup() {
-        
-    },
-    created() {
-        
-    },
-    mounted() {
-        
-    },
-    unmounted() {
-        
-    },
-    methods: {
-        
+  name: '',
+  components: {
+    
+  },
+  data() {
+    return {
+      credentials: {
+        email: '',
+        password: '',
+      }
     }
+  },
+  setup() {
+    
+  },
+  created() {
+    
+  },
+  mounted() {
+    mapActions(['login'])
+  },
+  unmounted() {
+    
+  },
+  methods: {
+    
+  }
 }
 </script>
--- front/src/views/accounts/store/index.js
+++ front/src/views/accounts/store/index.js
@@ -1,14 +1,14 @@
 // Vuex 조립
-import state from './state'
-import * as getters from './getters'
-import * as mutations from './mutations'
+// import state from './state'
+// import * as getters from './getters'
+// import * as mutations from './mutations'
 import * as actions from './actions'
 
 const accounts = {
   namespaced: true,
-  state,
-  getters,
-  mutations,
+//   state,
+//   getters,
+//   mutations,
   actions
 }
 
