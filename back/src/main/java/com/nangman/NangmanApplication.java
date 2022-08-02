68ff8e40438e80e678ecfb87baa4db3f5c51df61
unknown <orien93@naver.com>
2022-08-02 10:16:52 +0900

    [feature] Redis

--- back/src/main/java/com/nangman/NangmanApplication.java
+++ back/src/main/java/com/nangman/NangmanApplication.java
@@ -3,6 +3,7 @@ package com.nangman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -10,6 +11,7 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
+@EnableRedisRepositories
 public class NangmanApplication {
 	public static void main(String[] args) {
         SpringApplication.run(NangmanApplication.class, args);
