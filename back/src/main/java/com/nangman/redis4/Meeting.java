d5de0e3d00a0b91c8fa099338a8a01a203f23163
unknown <orien93@naver.com>
2022-08-03 16:31:02 +0900

    [feature] Redis4

---
+++ back/src/main/java/com/nangman/redis4/Meeting.java
@@ -0,0 +1,44 @@
+package com.nangman.redis4;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.util.Date;
+
+@RedisHash("Meetings")
+public class Meeting {
+
+    @Id
+    private String id;
+
+    private String title;
+
+    private Date startAt;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getStartAt() {
+        return startAt;
+    }
+
+    public void setStartAt(Date startAt) {
+        this.startAt = startAt;
+    }
+
+
+}
---
+++ back/src/main/java/com/nangman/redis4/MeetingRepository.java
@@ -0,0 +1,6 @@
+package com.nangman.redis4;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MeetingRepository extends CrudRepository<Meeting, String> {
+}
