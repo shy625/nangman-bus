9e6c2be37087f6f9e9b74a441b4aac6cf4fe68cd
bmyusharp <1004romeo@naver.com>
2022-08-03 23:34:43 +0900

    [chore] add mainpage components

---
+++ front/src/views/main/components/Nickname.vue
@@ -0,0 +1,37 @@
+<!-- 
+  룰렛을 돌렸는지 안 돌렸는지
+  4시 기준으로 초기화 할 때 접속을 했을때 기준 확인
+  하루에 두 번째 접속부터는 닉네임 GET으로 조회
+  store에 닉네임을 저장...
+ -->
+<template>
+  
+</template>
+<script>
+ export default {
+  name: '',
+  components: {
+    
+  },
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {
+    
+  },
+  created() {
+    
+  },
+  mounted() {
+    
+  },
+  unmounted() {
+    
+  },
+  methods: {
+    
+  }
+ }
+</script>
