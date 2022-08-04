8fbd619bcbabd20ac5f5b0fb237a6d00860232eb
Bermyu <1004romeo@naver.com>
2022-08-04 10:49:10 +0900

    [style] Add icon, logo 로고 파일 asset에 추가, icon 라이브러리 fontawesome 패키지에 추가, 그리고 Header.vue에 적용함

---
+++ front/src/views/main/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <a href="https://127.0.0.1:8000/">
+    <img src="@/assets/logo.png" alt="낭만버스 로고">
+  </a>
+  <a href="">
+    <font-awesome-icon icon="fa-solid fa-circle-user" />
+  </a>
+</template>
+<script setup>
+  import { useStore } from 'vuex'
+  import { ref, onMounted } from 'vue'
+  const store = useStore()
+  onMounted(() => {
+  })
+  const dataNameExample = ref({
+    
+  })
+  const fName = function (event) {
+    
+  }
+</script>
