65037f1e98a6feea912498cb9a64a95f19de5cb8
shy625 <thgusdud123@gmail.com>
2022-08-04 10:59:28 +0900

    [feature] Update socket config to use SockJS

--- back/src/main/java/com/nangman/socket/WebSocketConfig.java
+++ back/src/main/java/com/nangman/socket/WebSocketConfig.java
@@ -16,6 +16,8 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(chatHandler, "ws/chat")
-                .setAllowedOrigins("*");
+//                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 }
