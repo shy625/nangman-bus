971897595a66dfee5a12d5c84cbaf3ecd7cc0cdf
Bermyu <1004romeo@naver.com>
2022-08-08 17:59:37 +0900

    [chore] plus components

---
+++ front/src/views/components/NangmanBus.vue
@@ -0,0 +1,17 @@
+<template>
+  <img src="../../assets/bus-unclicked.png" alt="낭만버스정색">
+  <!-- <img src="../../assets/vus-clicked.png" alt="낭만버스행복"> -->
+</template>
+<script setup>
+  import { useStore } from 'vuex'
+  import { ref, onMounted } from 'vue'
+  const store = useStore()
+  onMounted(() => {
+  })
+  const dataNameExample = ref({
+    
+  })
+  const fName = function (event) {
+    
+  }
+</script>
