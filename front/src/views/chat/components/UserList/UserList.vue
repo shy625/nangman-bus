0aecd855e3e5bf88ab56e1921db11bc3d9a43ebe
woohree <woohree2@gmail.com>
2022-08-09 23:30:24 +0900

    [feature] user list cataclysm

--- front/src/views/chat/components/UserList/UserList.vue
+++ front/src/views/chat/components/UserList/UserList.vue
@@ -1,4 +1,6 @@
 <template>
+      <button @click="clickBbtn">임시버튼ㅋ</button>
+
   <div id="users" class="users">
     <div class="users-title">
       <div class="user-count">
@@ -15,7 +17,7 @@
           <img class="steer" src="../../../../assets/steer.png" alt="steerImg" width="68">
           <div class="cover">
             <div class="cover-title">
-              저 다음에 내려요
+              "저 이번에 내려요"
             </div>
             <div class="list-cover">
               <div class="getoff-list">
@@ -25,6 +27,7 @@
                 <div class="getoff-user">
                   가우르구라
                 </div>
+                <!-- 맨위 사람과 같은 사람 한번더 넣어줘야 자연스럽게 돔 -->
                 <div class="getoff-user">
                   헤라클레스
                 </div>
@@ -32,7 +35,7 @@
             </div>
           </div>
         </div>
-        <div id="me" class="user">
+        <!-- <div id="me" class="user">
           <div class="icon">
             O
           </div>
@@ -50,8 +53,43 @@
               흥덕지구 11단지 경남아너스빌
             </div>
           </div>
+        </div> -->
+        <div class="test-users">
+          <div class="test-user">
+            <div class="test-icon">
+              <!-- <img src="../../../../assets/seat2.png" alt="seat" class="seat-icon" width="96" height="96"> -->
+              <img src="../../../../assets/user-pink.png" alt="pink" class="user-icon">
+            </div>
+            <div class="test-name">
+              헤라클레스
+            </div>
+          </div>
+          <div class="test-user">
+            <div class="test-icon">
+              <img src="../../../../assets/user-yellow.png" alt="yellow" class="user-icon">
+            </div>
+            <div class="test-name">
+              홀리싯
+            </div>
+          </div>
+          <div class="test-user">
+            <div class="test-icon">
+              <img src="../../../../assets/user-red.png" alt="red" class="user-icon">
+            </div>
+            <div class="test-name">
+              캬오오옭
+            </div>
+          </div>
+          <div class="test-user">
+            <div class="test-icon">
+              <img src="../../../../assets/user-pinker.png" alt="pinker" class="user-icon">
+            </div>
+            <div class="test-name">
+              수퍼파워현규몬스터
+            </div>
+          </div>
         </div>
-        <div id="one" class="user">
+        <!-- <div id="two" class="user">
           <div class="icon">
             O
           </div>
@@ -68,25 +106,7 @@
               목감 지하차도 윤병찬 생가
             </div>
           </div>
-        </div>
-        <div id="two" class="user">
-          <div class="icon">
-            O
-          </div>
-          <div class="profile">
-            <div class="profile-nick">
-              <div class="nickname">
-                헤라클레스
-              </div>
-              <div class="birth">
-                
-              </div>
-            </div>
-            <div class="destination">
-              목감 지하차도 윤병찬 생가
-            </div>
-          </div>
-        </div>
+        </div> -->
       </el-scrollbar>
     </div>
   </div>
@@ -101,7 +121,7 @@ const data = ref({
 })
 
 onMounted(() => {
-  const user = document.querySelectorAll('.user')
+  const user = document.querySelectorAll('.test-user')
   const profileContainer = document.querySelector('#profileContainer')
   const users = document.querySelector('#users')
   const body = document.querySelector('body')
@@ -131,8 +151,121 @@ onMounted(() => {
     })
   })
 })
+// 유저 추가
+const clickBbtn = () => {
+  const addUser = document.createElement('div')
+  addUser.classList.add('test-user')
+  const addIcon = document.createElement('div')
+  addIcon.classList.add('test-icon')
+  const img = document.createElement('img')
+  img.src = `/src/assets/user-yellow.png`
+  img.alt = 'red'
+  img.classList.add('user-icon')
+  const addName = document.createElement('div')
+  addName.classList.add('test-name')
+  addName.innerText = '아몰라몰라'
+
+  const testUsers = document.querySelector('.test-users')
+  addUser.append(addIcon, addName)
+  addIcon.append(img)
+  testUsers.append(addUser)
+  addUser.classList.add('add-user')
+}
 </script>
 <style>
+.add-user {
+  animation: bounce 1s;
+}
+@keyframes bounce {
+  from,
+  60%,
+  75%,
+  90%,
+  to {
+    animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+  }
+
+  from {
+    opacity: 0;
+    transform: translate3d(0, 3000px, 0) scaleY(5);
+  }
+
+  60% {
+    opacity: 1;
+    transform: translate3d(0, -20px, 0) scaleY(0.9);
+  }
+
+  75% {
+    transform: translate3d(0, 10px, 0) scaleY(0.95);
+  }
+
+  90% {
+    transform: translate3d(0, -5px, 0) scaleY(0.985);
+  }
+
+  to {
+    transform: translate3d(0, 0, 0);
+  }
+}
+.test-users {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 28px;
+  row-gap: 20px;
+  margin-top: 48px;
+}
+.test-user {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.test-icon {
+  width: fit-content;
+  position: relative;
+}
+.user-icon {
+  width: 70px;
+  height: 70px;
+}
+/* .seat-icon {
+  position: absolute;
+  top: 8%;
+  left: 0;
+  opacity: .3;
+  z-index: -1;
+} */
+.test-name {
+  font-size: 1.1rem;
+}
+.steer {
+  animation: swing 4s infinite;
+}
+@keyframes swing {
+  5% {
+    -webkit-transform: rotate3d(0, 0, 1, 15deg);
+    transform: rotate3d(0, 0, 1, 15deg);
+  }
+
+  10% {
+    -webkit-transform: rotate3d(0, 0, 1, -10deg);
+    transform: rotate3d(0, 0, 1, -10deg);
+  }
+
+  15% {
+    -webkit-transform: rotate3d(0, 0, 1, 5deg);
+    transform: rotate3d(0, 0, 1, 5deg);
+  }
+
+  20% {
+    -webkit-transform: rotate3d(0, 0, 1, -5deg);
+    transform: rotate3d(0, 0, 1, -5deg);
+  }
+
+  25% {
+    -webkit-transform: rotate3d(0, 0, 1, 0deg);
+    transform: rotate3d(0, 0, 1, 0deg);
+  }
+}
 .arrow-profile-active {
   display: none !important;
 }
@@ -168,7 +301,7 @@ onMounted(() => {
   flex-grow: 1;
   position: relative;
 }
-.user-list::after {
+.user-list .el-scrollbar__wrap::after {
   content: '';
   position: absolute;
   left: 0;
@@ -180,9 +313,13 @@ onMounted(() => {
   border-left: 4px solid #F34949;
   border-right: 4px solid #F34949;
 }
+.user-list .el-scrollbar__wrap {
+  border-bottom: 4px solid #F34949;
+  border-radius: 10px;
+}
 .driver {
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   margin: 32px 40px;
 }
 .cover {
