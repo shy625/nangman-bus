e3707a13dab6f534ca87936bb5a73960c5205c50
hwi <orien93@naver.com>
2022-08-12 16:13:37 +0900

    [feature] getRandomBus

--- front/src/views/main/components/RandomBus.vue
+++ front/src/views/main/components/RandomBus.vue
@@ -6,7 +6,7 @@
       </div>
       <hr class="mostlybus-title-line">
     </div>
-    <div class="mostlybus-box">
+    <!-- <div class="mostlybus-box">
       <div class="mostlybus-content">
         지금 <span class="mostlybus-data">양재IC</span> 지나는 <span class="mostlybus-data">5100번</span> 버스는요,<br>
         <span class="mostlybus-status">아주 시끌벅적하네요!</span>
@@ -18,11 +18,41 @@
       <div class="mostlybus-content">
         지금 <span class="mostlybus-data">양재IC</span> 지나는 <span class="mostlybus-data">5100번</span> 버스는요,<br>
         <span class="mostlybus-status">아주 시끌벅적하네요!</span>
+      </div>
+    </div> -->
+    <div v-for="(bus, index) in buses.randomBus" :key="index">
+      <div class="mostlybus-content">
+        지금 <span class="mostlybus-data">{{bus.nodeName}}</span> 지나는 <span class="mostlybus-data">{{bus.routeNumber}}번</span> 버스는요,<br>
+        <span v-if="bus.type==0" >조금 <span>어색</span>해요!</span>
+        <span v-else > <span>시끌벅적</span>해요!</span>
       </div>
     </div>
   </div>
 </template>
 <script setup>
+import axios from "axios"
+import { ref, onMounted } from 'vue'
+import api from "../../../api/api"
+const buses = ref({
+  randomBus: []
+})
+
+onMounted(() => {
+  getRandomBus()
+})
+
+function getRandomBus() {
+  axios({
+    url: api.accounts.getrandombus(),
+    method: "get",
+  })
+    .then(res => {
+      buses.value.randomBus = res.data;
+    })
+    .catch(err => {
+    console.log("error")
+  })
+}
 
 </script>
 <style>
