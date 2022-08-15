fb1c8d0c5435507578d4832a0f8294cfbfdc3844
곽종환 <1004romeo@naver.com>
2022-08-15 13:18:38 +0900

    Update mutations.js

--- src/views/chat/store/mutations.js
+++ src/views/chat/store/mutations.js
@@ -8,3 +8,15 @@ export function SET_BOARDS_CREATE(state, boards) {
   state.boards = boards.reverse().slice(0, -1)
 }
 
+export function SET_SESSION_ID(state, sessionId) {
+  state.sessionId = sessionId
+  localStorage.setItem('sessionID', sessionId)
+}
+
+export function SET_ROOMS(state, rooms) {
+  state.rooms = rooms
+}
+
+export function SET_ROOM_INFO(state, roomInfo) {
+  state.roomInfo = roomInfo
+}
