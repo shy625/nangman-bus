a87855349f37ab32786eb6cf39a6df94b0068f8f
shy625 <thgusdud123@gmail.com>
2022-08-17 18:31:08 +0900

    [fix] Update joinRoom to fix user birthday check

--- back/src/main/java/com/nangman/api/dto/SocketDto.java
+++ back/src/main/java/com/nangman/api/dto/SocketDto.java
@@ -1,10 +1,7 @@
 package com.nangman.api.dto;
 
 import io.swagger.annotations.ApiModel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @ApiModel("Socket model")
 public class SocketDto {
@@ -17,6 +14,7 @@ public class SocketDto {
         private String message;
     }
 
+    @ToString
     @AllArgsConstructor
     @NoArgsConstructor
     public static class SubUserInOut {
